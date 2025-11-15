c4eaa882bcd05a25a1c7aa47cb6c2952daa8ec54
atoubaman <juniorliango01@gmail.com>
2025-11-15 04:38:55 -0500

    Fix homepage to show exactly 6 posts with View All button
    - Fix include error by using post.excerpt instead
    - Simplify post limiting logic
    - Add 'View All Posts' button at bottom of homepage
    - POSTS tab shows all posts with pagination

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763199280',resources: [
+    cacheName: 'chirpy-1763199532',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
