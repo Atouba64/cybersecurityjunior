120f535cea05b4c71077425a0cb789ade534c002
atoubaman <juniorliango01@gmail.com>
2025-11-16 01:06:22 -0500

    1116106am

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763273153',resources: [
+    cacheName: 'chirpy-1763273161',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
