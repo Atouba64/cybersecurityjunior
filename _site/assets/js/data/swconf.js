0cfc11a8c6217592b7d60cfacfb24727047721c5
atoubaman <juniorliango01@gmail.com>
2025-11-16 01:02:18 -0500

    Fix environment configuration in pages-deploy.yml
    - Move environment block after runs-on (correct YAML order)
    - Remove url reference that causes 'github-pages is not valid' error
    - Environment URL is handled automatically by deploy-pages action

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763272654',resources: [
+    cacheName: 'chirpy-1763272937',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
