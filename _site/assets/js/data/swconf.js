2b885ae335864b3303e6892e827b9ae5a925620f
atoubaman <juniorliango01@gmail.com>
2025-11-16 01:18:04 -0500

    Add GitHub Pages deployment workflow
    - Create deploy.yml workflow for automatic GitHub Pages deployment
    - Builds Jekyll site with all dependencies
    - Deploys to GitHub Pages on push to main/master branch
    - Fixes issue where pushes weren't deploying to live site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763273511',resources: [
+    cacheName: 'chirpy-1763273882',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
