0f1479700d3dd17395ace046857ba1de6bebcfe7
atoubaman <juniorliango01@gmail.com>
2025-11-20 12:52:53 -0500

    Remove duplicate jekyll.yml workflow, keep only pages-deploy.yml

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763660950',resources: [
+    cacheName: 'chirpy-1763661163',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
